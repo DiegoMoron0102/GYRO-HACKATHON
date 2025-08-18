2e2c6aa4331a6158793ec672c918b07899c8fa90
LaBalaElmo <jrttaquichiri@gmail.com>
2025-08-18 00:08:02 -0400

    update contracts ids

--- packages/gyro/src/index.ts
+++ packages/gyro/src/index.ts
@@ -34,62 +34,27 @@ if (typeof window !== 'undefined') {
 export const networks = {
   testnet: {
     networkPassphrase: "Test SDF Network ; September 2015",
-    contractId: "CAOSVKNJ54XTRNLPBS5HBSY2YVIAZYPM2CBQOMLVXOSL7GA6DFRT3AJY",
+    contractId: "CBCBNDGRKJ6EKR723WQEGTPK6YB6YLVSZRWH55P2PUVEI3M635MLXOYY",
   }
 } as const
 
-export type DataKey = {tag: "Balance", values: readonly [string, AssetType]} | {tag: "Transactions", values: readonly [string]};
+export type DataKey = {tag: "User", values: readonly [string]};
 
-export type AssetType = {tag: "Bs", values: void} | {tag: "USDC", values: void};
+export type Role = {tag: "Owner", values: void} | {tag: "Admins", values: void} | {tag: "User", values: void};
 
-export type TransactionType = {tag: "Deposit", values: void} | {tag: "Transfer", values: void};
-
-
-export interface Transaction {
-  amount: u32;
-  asset_type: AssetType;
-  date: string;
-  from: string;
-  to: string;
-  transaction_type: TransactionType;
-  tx_id: string;
-}
-
-export const TransactionError = {
-  1: {message:"DuplicateTx"},
-  2: {message:"InsufficientBalance"},
-  3: {message:"BalanceDoesNotExist"},
-  4: {message:"ContractPaused"},
-  5: {message:"InsufficientLiquidityFund"},
-  6: {message:"TransactionNotFound"},
-  7: {message:"TransactionIsEmpty"}
+export const UserError = {
+  1: {message:"NotAuthorized"},
+  2: {message:"AlreadyRegistered"},
+  3: {message:"NotRegistered"},
+  4: {message:"OwnerNotSet"},
+  5: {message:"AlreadyAdmin"}
 }
 
 export interface Client {
   /**
-   * Construct and simulate a register_balance transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Construct and simulate a register_user transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  register_balance: ({user}: {user: string}, options?: {
-    /**
-     * The fee to pay for the transaction. Default: BASE_FEE
-     */
-    fee?: number;
-
-    /**
-     * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
-     */
-    timeoutInSeconds?: number;
-
-    /**
-     * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
-     */
-    simulate?: boolean;
-  }) => Promise<AssembledTransaction<null>>
-
-  /**
-   * Construct and simulate a transfer transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
-   */
-  transfer: ({from, to, asset_type, amount, date, tx_id}: {from: string, to: string, asset_type: AssetType, amount: u32, date: string, tx_id: string}, options?: {
+  register_user: ({user}: {user: string}, options?: {
     /**
      * The fee to pay for the transaction. Default: BASE_FEE
      */
@@ -107,9 +72,9 @@ export interface Client {
   }) => Promise<AssembledTransaction<Result<void>>>
 
   /**
-   * Construct and simulate a withdraw transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Construct and simulate a add_admin transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  withdraw: ({user, asset_type, amount, date, tx_id}: {user: string, asset_type: AssetType, amount: u32, date: string, tx_id: string}, options?: {
+  add_admin: ({admin}: {admin: string}, options?: {
     /**
      * The fee to pay for the transaction. Default: BASE_FEE
      */
@@ -127,9 +92,9 @@ export interface Client {
   }) => Promise<AssembledTransaction<Result<void>>>
 
   /**
-   * Construct and simulate a get_user_balance transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Construct and simulate a get_admins transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  get_user_balance: ({user, asset_type}: {user: string, asset_type: AssetType}, options?: {
+  get_admins: (options?: {
     /**
      * The fee to pay for the transaction. Default: BASE_FEE
      */
@@ -144,12 +109,12 @@ export interface Client {
      * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
      */
     simulate?: boolean;
-  }) => Promise<AssembledTransaction<Result<u32>>>
+  }) => Promise<AssembledTransaction<Array<string>>>
 
   /**
-   * Construct and simulate a get_tx_id transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Construct and simulate a is_admin transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  get_tx_id: ({user, tx_id}: {user: string, tx_id: string}, options?: {
+  is_admin: ({user}: {user: string}, options?: {
     /**
      * The fee to pay for the transaction. Default: BASE_FEE
      */
@@ -164,12 +129,12 @@ export interface Client {
      * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
      */
     simulate?: boolean;
-  }) => Promise<AssembledTransaction<Result<Transaction>>>
+  }) => Promise<AssembledTransaction<boolean>>
 
   /**
-   * Construct and simulate a get_transactions transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   * Construct and simulate a is_user transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  get_transactions: ({user}: {user: string}, options?: {
+  is_user: ({user}: {user: string}, options?: {
     /**
      * The fee to pay for the transaction. Default: BASE_FEE
      */
@@ -184,7 +149,7 @@ export interface Client {
      * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
      */
     simulate?: boolean;
-  }) => Promise<AssembledTransaction<Array<Transaction>>>
+  }) => Promise<AssembledTransaction<boolean>>
 
 }
 export class Client extends ContractClient {
@@ -207,26 +172,22 @@ export class Client extends ContractClient {
   constructor(public readonly options: ContractClientOptions) {
     super(
       new ContractSpec([ "AAAAAAAAAAAAAAANX19jb25zdHJ1Y3RvcgAAAAAAAAEAAAAAAAAABW93bmVyAAAAAAAAEwAAAAA=",
-        "AAAAAAAAAAAAAAAQcmVnaXN0ZXJfYmFsYW5jZQAAAAEAAAAAAAAABHVzZXIAAAATAAAAAA==",
-        "AAAAAAAAAAAAAAAIdHJhbnNmZXIAAAAGAAAAAAAAAARmcm9tAAAAEwAAAAAAAAACdG8AAAAAABMAAAAAAAAACmFzc2V0X3R5cGUAAAAAB9AAAAAJQXNzZXRUeXBlAAAAAAAAAAAAAAZhbW91bnQAAAAAAAQAAAAAAAAABGRhdGUAAAAQAAAAAAAAAAV0eF9pZAAAAAAAABAAAAABAAAD6QAAA+0AAAAAAAAH0AAAABBUcmFuc2FjdGlvbkVycm9y",
-        "AAAAAAAAAAAAAAAId2l0aGRyYXcAAAAFAAAAAAAAAAR1c2VyAAAAEwAAAAAAAAAKYXNzZXRfdHlwZQAAAAAH0AAAAAlBc3NldFR5cGUAAAAAAAAAAAAABmFtb3VudAAAAAAABAAAAAAAAAAEZGF0ZQAAABAAAAAAAAAABXR4X2lkAAAAAAAAEAAAAAEAAAPpAAAD7QAAAAAAAAfQAAAAEFRyYW5zYWN0aW9uRXJyb3I=",
-        "AAAAAAAAAAAAAAAQZ2V0X3VzZXJfYmFsYW5jZQAAAAIAAAAAAAAABHVzZXIAAAATAAAAAAAAAAphc3NldF90eXBlAAAAAAfQAAAACUFzc2V0VHlwZQAAAAAAAAEAAAPpAAAABAAAB9AAAAAQVHJhbnNhY3Rpb25FcnJvcg==",
-        "AAAAAAAAAAAAAAAJZ2V0X3R4X2lkAAAAAAAAAgAAAAAAAAAEdXNlcgAAABMAAAAAAAAABXR4X2lkAAAAAAAAEAAAAAEAAAPpAAAH0AAAAAtUcmFuc2FjdGlvbgAAAAfQAAAAEFRyYW5zYWN0aW9uRXJyb3I=",
-        "AAAAAAAAAAAAAAAQZ2V0X3RyYW5zYWN0aW9ucwAAAAEAAAAAAAAABHVzZXIAAAATAAAAAQAAA+oAAAfQAAAAC1RyYW5zYWN0aW9uAA==",
-        "AAAAAgAAAAAAAAAAAAAAB0RhdGFLZXkAAAAAAgAAAAEAAAAAAAAAB0JhbGFuY2UAAAAAAgAAABMAAAfQAAAACUFzc2V0VHlwZQAAAAAAAAEAAAAAAAAADFRyYW5zYWN0aW9ucwAAAAEAAAAT",
-        "AAAAAgAAAAAAAAAAAAAACUFzc2V0VHlwZQAAAAAAAAIAAAAAAAAAAAAAAAJCcwAAAAAAAAAAAAAAAAAEVVNEQw==",
-        "AAAAAgAAAAAAAAAAAAAAD1RyYW5zYWN0aW9uVHlwZQAAAAACAAAAAAAAAAAAAAAHRGVwb3NpdAAAAAAAAAAAAAAAAAhUcmFuc2Zlcg==",
-        "AAAAAQAAAAAAAAAAAAAAC1RyYW5zYWN0aW9uAAAAAAcAAAAAAAAABmFtb3VudAAAAAAABAAAAAAAAAAKYXNzZXRfdHlwZQAAAAAH0AAAAAlBc3NldFR5cGUAAAAAAAAAAAAABGRhdGUAAAAQAAAAAAAAAARmcm9tAAAAEwAAAAAAAAACdG8AAAAAABMAAAAAAAAAEHRyYW5zYWN0aW9uX3R5cGUAAAfQAAAAD1RyYW5zYWN0aW9uVHlwZQAAAAAAAAAABXR4X2lkAAAAAAAAEA==",
-        "AAAABAAAAAAAAAAAAAAAEFRyYW5zYWN0aW9uRXJyb3IAAAAHAAAAAAAAAAtEdXBsaWNhdGVUeAAAAAABAAAAAAAAABNJbnN1ZmZpY2llbnRCYWxhbmNlAAAAAAIAAAAAAAAAE0JhbGFuY2VEb2VzTm90RXhpc3QAAAAAAwAAAAAAAAAOQ29udHJhY3RQYXVzZWQAAAAAAAQAAAAAAAAAGUluc3VmZmljaWVudExpcXVpZGl0eUZ1bmQAAAAAAAAFAAAAAAAAABNUcmFuc2FjdGlvbk5vdEZvdW5kAAAAAAYAAAAAAAAAElRyYW5zYWN0aW9uSXNFbXB0eQAAAAAABw==" ]),
+        "AAAAAAAAAAAAAAANcmVnaXN0ZXJfdXNlcgAAAAAAAAEAAAAAAAAABHVzZXIAAAATAAAAAQAAA+kAAAPtAAAAAAAAB9AAAAAJVXNlckVycm9yAAAA",
+        "AAAAAAAAAAAAAAAJYWRkX2FkbWluAAAAAAAAAQAAAAAAAAAFYWRtaW4AAAAAAAATAAAAAQAAA+kAAAPtAAAAAAAAB9AAAAAJVXNlckVycm9yAAAA",
+        "AAAAAAAAAAAAAAAKZ2V0X2FkbWlucwAAAAAAAAAAAAEAAAPqAAAAEw==",
+        "AAAAAAAAAAAAAAAIaXNfYWRtaW4AAAABAAAAAAAAAAR1c2VyAAAAEwAAAAEAAAAB",
+        "AAAAAAAAAAAAAAAHaXNfdXNlcgAAAAABAAAAAAAAAAR1c2VyAAAAEwAAAAEAAAAB",
+        "AAAAAgAAAAAAAAAAAAAAB0RhdGFLZXkAAAAAAQAAAAEAAAAAAAAABFVzZXIAAAABAAAAEw==",
+        "AAAAAgAAAAAAAAAAAAAABFJvbGUAAAADAAAAAAAAAAAAAAAFT3duZXIAAAAAAAAAAAAAAAAAAAZBZG1pbnMAAAAAAAAAAAAAAAAABFVzZXI=",
+        "AAAABAAAAAAAAAAAAAAACVVzZXJFcnJvcgAAAAAAAAUAAAAAAAAADU5vdEF1dGhvcml6ZWQAAAAAAAABAAAAAAAAABFBbHJlYWR5UmVnaXN0ZXJlZAAAAAAAAAIAAAAAAAAADU5vdFJlZ2lzdGVyZWQAAAAAAAADAAAAAAAAAAtPd25lck5vdFNldAAAAAAEAAAAAAAAAAxBbHJlYWR5QWRtaW4AAAAF" ]),
       options
     )
   }
   public readonly fromJSON = {
-    register_balance: this.txFromJSON<null>,
-        transfer: this.txFromJSON<Result<void>>,
-        withdraw: this.txFromJSON<Result<void>>,
-        get_user_balance: this.txFromJSON<Result<u32>>,
-        get_tx_id: this.txFromJSON<Result<Transaction>>,
-        get_transactions: this.txFromJSON<Array<Transaction>>
+    register_user: this.txFromJSON<Result<void>>,
+        add_admin: this.txFromJSON<Result<void>>,
+        get_admins: this.txFromJSON<Array<string>>,
+        is_admin: this.txFromJSON<boolean>,
+        is_user: this.txFromJSON<boolean>
   }
 }
